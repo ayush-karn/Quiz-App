f2660da9a97296af54ffe7f660630f9389213cfa
ayush_ip1-at-857478475153 <ayushkarn2127@gmail.com>
2021-06-21 05:09:34 +0530

    first commit

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -22,6 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin', admin.site.urls),
     path('' , include('accounts.urls')),
-    path('' , include('app.urls'))
-
+    path('' , include('app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
